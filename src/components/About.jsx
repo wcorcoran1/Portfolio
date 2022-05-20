180541b829fbbbf342a6fef2e6a5367fbe94c569
Wesley Corcoran <wesleycorcoran@Wesleys-MacBook-Air.local>
2022-05-20 09:58:00 -0500

    stranger things deploy link added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,8 +14,9 @@ return(
         </h1>
         <p>
          I am a software engineer, gamer and traveler. 
-         I am a Fullstack Academy graduate and that sparked my passion for creating things through software development.
-         I am looking for the opportunity to continue to grow as a developer and as professional. I  
+         I am a Fullstack Academy graduate and I learned a lot in the three months of the class. 
+         Fullstack helped sparked my passion for creating things through software development. 
+         I am looking for the opportunity to continue to grow as a developer and as professional.    
         </p>
          </div>  
     
